bb643b250867020fe3cc375ab984c7c892bd74aa
anyaoboiydno <nastenkoanya95@gmail.com>
2026-03-10 22:14:25 +0200

    Done lab 2 var 3

--- submission2/main.py
+++ submission2/main.py
@@ -1,1 +1,21 @@
-# Implement task here
+start = int(input("Введіть початкове число інтервалу: "))
+end = int(input("Введіть кінцеве число інтервалу: "))
+
+print(f"\nПрості числа в інтервалі від {start} до {end}:")
+
+# Перебір чисел у заданому діапазоні
+for num in range(start, end + 1):
+    # Прості числа завжди більші за 1
+    if num > 1:
+        is_prime = True
+        
+        # Перевіряємо наявність дільників
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break  # Якщо знайдено дільник, перериваємо цикл
+                
+        # Якщо число залишилось простим, виводимо його
+        if is_prime:
+            print(num, end=" ")
+            
