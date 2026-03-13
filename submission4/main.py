26d4b76bef7427c085f94b3696d8d2e2a6dd3cb8
Поліна Полєшко <polinapolesko63@gmail.com>
2026-03-13 20:21:41 +0200

    Refactor factorial function usage and remove input prompts
    Removed user input handling and replaced it with direct calls to factorial function.

--- submission4/main.py
+++ submission4/main.py
@@ -1,29 +1,12 @@
 def factorial(n):
     if n == 0:
-        return 1 
-    
+        return 1
+
     result = 1
     for i in range(1, n + 1):
-        result = result * i 
-    return result 
+        result = result * i
+    return result
 
-print("Обчислення факторіала числа")
-print("Вводьте числа. Щоб завершити програму, напишіть 'stop'")
-try: 
 
-    while True: 
-        value = input("Введіть число: ")  
-
-        if value.lower() == "stop":  
-            print("Програму завершено")
-            break 
-
-        num = int(value) 
-
-        if num < 0: 
-            print("Факторіал для від’ємних чисел не існує")
-            continue
-
-        print(num, "! =", factorial(num))
-except: 
-     print("помилка, введіть коректні числа")
+print(factorial(5))
+print(factorial(8))
