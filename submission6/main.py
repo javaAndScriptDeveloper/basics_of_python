c31571e1e546ffa6b5b5db3b60f4171a03323782
Поліна Полєшко <polinapolesko63@gmail.com>
2026-03-14 15:31:57 +0200

    Update main.py

--- submission6/main.py
+++ submission6/main.py
@@ -5,7 +5,7 @@ import os
 
 
 def create_initial_files():
-    
+
     initial_students = [
         {"name": "Катя", "age": 20, "faculty": "Економіка"},
         {"name": "Марія", "age": 19, "faculty": "Економіка"},
@@ -34,7 +34,7 @@ def task_json():
     with open("students.json", "r", encoding="utf-8") as f:
         students = json.load(f)
 
-    students.append({"name": "Анна", "age": 22, "faculty": "Інформатика"})  
+    students.append({"name": "Анна", "age": 22, "faculty": "Інформатика"})
     students[0]["faculty"] = "ФТІ"
 
     students = [s for s in students if s["name"] != "Марія"]
@@ -42,7 +42,7 @@ def task_json():
     with open("students_updated.json", "w", encoding="utf-8") as f:
         json.dump(students, f, ensure_ascii=False, indent=2)
 
-    print("Дані JSON оброблено та збережено у students_updated.json\n")
+    print("Дані JSON оброблено та збережено y students_updated.json\n")
 
 
 def task_csv():
@@ -50,6 +50,7 @@ def task_csv():
     students_csv = []
 
     with open("students.csv", "r", encoding="utf-8") as f:
+
         reader = csv.DictReader(f)
 
         for row in reader:
@@ -76,7 +77,7 @@ def task_excel():
     df_sorted = df_filtered.sort_values(by="age", ascending=True)
 
     df_sorted.to_excel("students_processed.xlsx", index=False)
-
+    
     print("Дані Excel відфільтровано та збережено у students_processed.xlsx\n")
 
 
@@ -86,18 +87,16 @@ def individual_task_2(new_name, new_age, new_faculty):
         with open("students.json", "r", encoding="utf-8") as f:
             data = json.load(f)
 
-        new_entry = {
-            "name": new_name,
-            "age": new_age,
-            "faculty": new_faculty
-        }
-
+        new_entry = {"name": new_name, "age": new_age, "faculty": new_faculty}
         data.append(new_entry)
 
         with open("students.json", "w", encoding="utf-8") as f:
             json.dump(data, f, ensure_ascii=False, indent=2)
 
-        print(f"Студент {new_name} успішно доданий до students.json\n")
+        with open("output.json", "w", encoding="utf-8") as f:
+            json.dump(data, f, ensure_ascii=False, indent=2)
+
+        print(f"Студент {new_name} успішно доданий до students.json та output.json\n")
 
     except Exception as e:
         print(f"Виникла помилка: {e}")
@@ -110,3 +109,4 @@ task_csv()
 task_excel()
 
 individual_task_2("Сергій", 24, "ФМ")
+
