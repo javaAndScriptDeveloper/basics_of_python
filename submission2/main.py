f2347eda41ded2a2dcbcfe75d87e9af624f2dc5e
Ростанець Ірина ІК-44 <ryskat12@gmail.com>
2026-03-14 22:34:46 +0200

    lab2_var3

--- submission2/main.py
+++ submission2/main.py
@@ -1,1 +1,23 @@
 # Implement task here
+import sys
+
+def main():
+    input_data = sys.stdin.read().split()
+
+    start = int(input_data[0])
+    end = int(input_data[1])
+
+    for num in range(start, end+1):
+        if num < 2:
+            continue
+
+        is_prime = True
+        for i in range(2, num):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            print(num, end=" ")
+
+if __name__ == '__main__':
+    main()
