8e80a69e1c152d6973e8485ed7ce34ad7f649e79
Madeline <dancent124@gmail.com>
2026-03-16 01:54:07 +0200

    Update main.py

--- submission1/main.py
+++ submission1/main.py
@@ -1,1 +1,12 @@
-# Implement task here
+a = float(input("Enter the first number: "))
+b = float(input("Enter another number: "))
+
+sum = a + b
+diff = a - b
+divide = a / b
+multiply = a * b
+
+print("Sum:", sum)
+print("Difference:", diff)
+print("Division:", divide)
+print("Multiplication:", multiply)
