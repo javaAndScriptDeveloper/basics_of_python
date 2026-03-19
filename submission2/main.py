efc72704bda554634b9d903d051c6089f82e470d
PrOl07 <protasov.oleksandr@lll.kpi.ua>
2026-03-19 14:22:50 +0200

    lab2 var1

--- submission2/main.py
+++ submission2/main.py
@@ -1,1 +1,14 @@
 # Implement task here
+# Лабораторна робота №2 з теми Використання операторів умовного виконання та циклів у мові
+# програмування Python.
+# Варіант 1:
+# Введення початкових даних
+first_element = float(input())
+diff = float(input())
+element_count = int(input())
+
+# За допомогою циклу for обчислюємо кожен елемент прогресії
+for i in range(0, element_count):
+    print(first_element)
+    first_element = first_element + diff
+
