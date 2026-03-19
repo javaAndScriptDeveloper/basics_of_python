caa85a04d713e8ad3bb0b75d540a982be03caadc
Ivan Vitkovskiy <cloudfly97@icloud.com>
2026-03-19 12:58:28 +0200

    Lab9 Vitkovskiy Ivan IM-44

--- submission9/main.py
+++ submission9/main.py
@@ -1,3 +1,20 @@
 # Лабораторна робота 9: Регулярні вирази
 
-# Реалізуйте завдання тут
+import re
+
+try:
+
+    user_input = input()
+    
+    if not user_input:
+        raise ValueError("Помилка! Рядок порожній")
+    
+    cleaned_text = re.sub(r'\s+', ' ', user_input)
+    
+   
+    print(cleaned_text.strip())
+
+except ValueError as err:
+    print(err)
+except EOFError:
+    print("Помилка! Немає вхідних даних")
