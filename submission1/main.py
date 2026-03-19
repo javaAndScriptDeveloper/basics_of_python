b33e8e09b9e13f46238d4785eb36f31a6d7e9868
oneshotsss <xxvlad20007xx@gmail.com>
2026-03-19 21:06:15 +0200

    lab1 var4

--- submission1/main.py
+++ submission1/main.py
@@ -1,1 +1,13 @@
-# Implement task here
+# оголошуємо дві зміні а і б, зразу перетворюмо тип даних зі стрінга в флоат
+a = float(input("Enter a number: "))
+b = float(input("Enter another number: "))
+
+print()#просто шоб віділити
+
+#основні операції
+print(f"addition: {a+b:g} \nsubtraction: {a-b:g} \nmultiplication: {a*b:g} \ndivision: {a/b:g}")#додав віднім множ діленя
+
+
+# Програма для множення двох введених чисел
+print(float(input("pershe: ")) * float(input("druge: ")))
+
