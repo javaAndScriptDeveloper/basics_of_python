3afc684be458b7962862448e9d8eada728f9e07f
Ivan Vitkovskiy <cloudfly97@icloud.com>
2026-03-19 13:35:47 +0200

    lab5_var3 Vitkovskiy Ivan IM-44

--- submission5/main.py
+++ submission5/main.py
@@ -20,3 +20,30 @@ new_line = "Новий рядок додано"
 new_content = "Файл перезаписано"
 
 # Реалізуйте завдання тут
+import os
+
+
+if not os.path.exists(input_file):
+    print("Файл не знайдено")
+else:
+    total = 0
+    try:
+        with open(input_file, 'r', encoding='utf-8') as infile:
+           
+            content = infile.read()
+           
+            data = content.split()
+            
+            for item in data:
+                total += int(item)
+        
+        with open(output_file, 'w', encoding='utf-8') as outfile:
+            outfile.write(str(total))
+            
+        print(total/5)
+
+    except ValueError:
+        print("Помилка: у файлі не число")
+    except Exception as e:
+        print(f"Виникла помилка: {e}")
+            
