3b45cf0a53a16ec0bf2c7aed921b4a7ffef73b3a
Derelckol <skaleckiy1507@gmail.com>
2026-03-23 00:50:25 +0200

    lab8

--- submission8/main.py
+++ submission8/main.py
@@ -1,3 +1,18 @@
 # Лабораторна робота 8: Основи ООП
 
 # Реалізуйте завдання тут
+class Employee:
+    def __init__(self, surname, name, age, position):
+        self.surname = surname
+        self.name = name
+        self.age = age
+        self.position = position
+    def display(self):
+        print(self.surname, self.name, self.age, self.position)
+
+surname = input("Enter surname: ")
+name = input("Enter name: ")
+age = input("Enter age: ")
+position = input("Enter position: ")
+employee = Employee(surname, name, age, position)
+employee.display()
