254a92d813ee37a9791f736f0656563abeea9bda
MiteRute <lilisehome3@gmail.com>
2026-03-25 11:26:16 +0200

    lab-8

--- submission8/main.py
+++ submission8/main.py
@@ -1,3 +1,24 @@
 # Лабораторна робота 8: Основи ООП
 
 # Реалізуйте завдання тут
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def calculate_area(self):
+        return self.length * self.width
+
+    def calculate_perimeter(self):
+        return 2 * (self.length + self.width)
+
+l = float(input())
+w = float(input())
+
+rect = Rectangle(l, w)
+
+area = rect.calculate_area()
+perimeter = rect.calculate_perimeter()
+
+print(f"Площа: {area}")
+print(f"Периметр: {perimeter}")
