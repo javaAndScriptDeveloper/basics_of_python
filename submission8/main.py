80a8c3c8cdb370a85e3a234713ba9baa9d0c0fea
kanishevska <kanishevska-yurkovska.olha@lll.kpi.ua>
2026-04-10 12:47:39 +0300

    LR8_2

--- submission8/main.py
+++ submission8/main.py
@@ -1,21 +1,17 @@
 # Лабораторна робота 8: Основи ООП
 
 # Реалізуйте завдання тут
-# Оголошення класу CalendarEvent
 class CalendarEvent:
-    # Конструктор класу
     def __init__(self, title, date, time):
-        self.title = title      # назва події
-        self.date = date        # дата
-        self.time = time        # час
+        self.title = title
+        self.date = date
+        self.time = time
 
-    # Метод для виведення інформації
     def show_event(self):
-        print("Подія:", self.title)
-        print("Дата:", self.date)
-        print("Час:", self.time)
+        print(f"Подія: {self.title}")
+        print(f"Дата: {self.date}")
+        print(f"Час: {self.time}")
 
-    # Метод для редагування події
     def edit_event(self, new_title=None, new_date=None, new_time=None):
         if new_title:
             self.title = new_title
@@ -25,14 +21,12 @@ class CalendarEvent:
             self.time = new_time
 
 
-# Створення події
 event1 = CalendarEvent("Зустріч", "10.04.2026", "14:00")
 
 print("Початкові дані:")
 event1.show_event()
 
-# Редагування події
 event1.edit_event(new_title="Зустріч з викладачем", new_time="15:00")
 
-print("\nПісля редагування:")
+print("Після редагування:")
 event1.show_event()
