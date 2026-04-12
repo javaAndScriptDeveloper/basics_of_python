583fa23f1cc5ee49e2a8baa2a403b76bcb0ad1c9
coldat1k <yrasav2007@gmail.com>
2026-04-13 00:36:22 +0300

    completed lab2_var9

--- submission2/main.py
+++ submission2/main.py
@@ -1,1 +1,14 @@
-# Implement task here
+def main():
+    try:
+        year = int(input())
+        
+        if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+            print("Рік високосний")
+        else: 
+            print("Рік звичайний")
+            
+    except ValueError:
+        print("Помилка: Будь ласка, введіть ціле число.")
+
+if __name__ == "__main__":
+    main()
