d4c47e992296ece9b207646a5dbb2513f3fc910b
coldat1k <yrasav2007@gmail.com>
2026-04-13 00:41:14 +0300

    fixed name

--- submission2/main.py
+++ submission2/main.py
@@ -5,7 +5,7 @@ def main():
         if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
             print("Рік високосний")
         else: 
-            print("Рік звичайний")
+            print("Не високосний")
             
     except ValueError:
         print("Помилка: Будь ласка, введіть ціле число.")
