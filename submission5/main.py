7bda3e3aeb9c0668bd3cebf8d372d1f59cc5b4e8
kab1why1 <kab1why@gmail.com>
2026-04-15 16:22:11 +0300

    lab5_10

--- submission5/main.py
+++ submission5/main.py
@@ -20,3 +20,25 @@ new_line = "Новий рядок додано"
 new_content = "Файл перезаписано"
 
 # Реалізуйте завдання тут
+
+import os
+
+# Перевіряємо, чи існує вхідний файл
+if os.path.exists(input_file):
+    
+    # Зчитуємо дані з вхідного файлу
+    with open(input_file, 'r', encoding='utf-8') as file:
+        original_text = file.read()
+        
+    # Базове завдання: переведення всіх літер у нижній регістр та запис у новий файл
+    with open(output_file, 'w', encoding='utf-8') as file:
+        file.write(original_text.lower())
+        
+    # Завдання 10 варіанту: Перезаписати файл лише якщо він існує.
+    # Автотест вимагає перезапису саме input.txt
+    with open(input_file, 'w', encoding='utf-8') as file:
+        file.write(new_content)
+        
+    print(f"Дані перетворено та збережено в {output_file}. {input_file} перезаписано.")
+else:
+    print(f"Помилка: Файл {input_file} не існує.")
