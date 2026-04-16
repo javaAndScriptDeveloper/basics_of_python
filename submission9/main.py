26dee5dd07abc2ffe61442ce2ef246eb1f6a987f
Kinosura666 <kornievskiy.leha@gmail.com>
2026-04-16 17:44:33 +0300

    lab9_var9

--- submission9/main.py
+++ submission9/main.py
@@ -1,3 +1,29 @@
 # Лабораторна робота 9: Регулярні вирази
 
 # Реалізуйте завдання тут
+import re
+import sys
+
+def is_valid_ip(ip):
+    if not re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", ip):
+        return False
+    
+    parts = ip.split('.')
+    for part in parts:
+        if not 0 <= int(part) <= 255:
+            return False
+    return True
+
+def main():
+    input_data = sys.stdin.read().strip()
+    
+    if not input_data:
+        return
+
+    if is_valid_ip(input_data):
+        print(f"IP {input_data} is valid")
+    else:
+        print(f"IP {input_data} is invalid")
+
+if __name__ == "__main__":
+    main()
