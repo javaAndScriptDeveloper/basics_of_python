f6d1ad9f154bf7de2a3af2e97c07a399b59e360e
oneshotsss <xxvlad20007xx@gmail.com>
2026-04-22 14:53:02 +0300

    qwe

--- submission5/main.py
+++ submission5/main.py
@@ -6,17 +6,23 @@ input_file = "input.txt"
 # Шлях до вихідного файлу
 output_file = "output.txt"
 
-# Слово для пошуку (варіант 5)
-word_to_find = "Python"
 
-# Слово для заміни та нове слово (варіант 6)
-word_to_replace = "World"
-replacement_word = "Ukraine"
+#варіант 3
+'''Написати програму, яка зчитує дані з текстового файлу та записує їх у новий
+файл, виконуючи певні перетворення (наприклад, переведення всіх літер у
+нижній регістр).'''
 
-# Новий рядок для додавання (варіант 7)
-new_line = "Новий рядок додано"
+# відкриваємо файл і зчитуємо числа
+with open("input.txt", "r") as file:
+    numbers = [float(line.strip()) for line in file if line.strip()]
 
-# Новий вміст для перезапису (варіант 10)
-new_content = "Файл перезаписано"
+# перевірка щоб не було ділення на 0
+if numbers:
+    average = sum(numbers) / len(numbers)
 
-# Реалізуйте завдання тут
+    with open("output.txt", "w", encoding="utf-8") as out_file:
+        out_file.write(f"Середнє значення: {average}\n")
+
+    print("Готово! Результат записано в output.txt")
+else:
+    print("Файл порожній або немає чисел")
