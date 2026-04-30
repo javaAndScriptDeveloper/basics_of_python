8ca7c02b5b2d5dfa37eb6f669e82ea1a717fee45
davydovychnikita-ip42-rgb <davydovych.nikita-ip42@edu.kpi.ua>
2026-04-30 16:48:59 +0300

    Update main.py

--- submission3/main.py
+++ submission3/main.py
@@ -21,7 +21,7 @@ value_to_find = 30
 # Реалізуйте завдання тут
 print("Початковий словник:", people)
 
-if len(products_prices) > 0:
+if len(people) > 0:
     min_value = min(people.values())
     print("Найменше значення у словнику:", min_value)
 else:
