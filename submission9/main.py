859cdc98576b713edb7e92f0979959d58de7f70d
shlstd <shelest.dasha.24680@gmail.com>
2026-04-30 16:41:59 +0300

    Update main.py

--- submission9/main.py
+++ submission9/main.py
@@ -1,3 +1,13 @@
 # Лабораторна робота 9: Регулярні вирази
 
 # Реалізуйте завдання тут
+import re
+
+text=input()
+
+pattern = r'\b[A-ZА-ЯІЇЄҐ][a-zа-яіїєґ]*\b'
+
+result = re.findall(pattern, text)
+
+print("Слова з великої літери:")
+print(result)
