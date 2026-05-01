453316d5f78d23ea39bc5d3391a316ba0b225083
locker13312 <litvinenko7b@gmail.com>
2026-05-01 13:32:13 +0300

    Lab 5 final

--- submission5/main.py
+++ submission5/main.py
@@ -20,3 +20,19 @@ new_line = "Новий рядок додано"
 new_content = "Файл перезаписано"
 
 # Реалізуйте завдання тут
+
+import os
+
+input_filename = 'input.txt'
+output_filename = 'output.txt'
+
+if os.path.exists(input_filename):
+    with open(input_filename, 'r', encoding='utf-8') as infile:
+        lines = infile.readlines()
+
+    processed_lines = [line for line in lines if line.strip() != '']
+
+    with open(output_filename, 'w', encoding='utf-8') as outfile:
+        outfile.writelines(processed_lines)
+else:
+    pass
