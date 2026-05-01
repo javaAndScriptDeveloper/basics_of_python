9847998f2cc891b84d5f8cb5889ee09a3d744af0
mvasa134 <mvasa134@gmail.com>
2026-05-01 13:11:15 +0300

    Update main.py

--- submission9/main.py
+++ submission9/main.py
@@ -1,3 +1,12 @@
 # Лабораторна робота 9: Регулярні вирази
 
 # Реалізуйте завдання тут
+import re
+def clean(text):
+    text = re.sub(r'\d', '#', text)
+    return text
+inp=input("Введіть текст для очистки від #")
+if not inp.strip():
+        print("Помилка: Ви нічого не ввели!")
+else:
+    print(clean(inp))
