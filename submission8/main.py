58ce5cf3109a9ddac13056b7778cd4670bd3b9f7
coldat1k <yrasav2007@gmail.com>
2026-05-04 20:11:49 +0300

    L8

--- submission8/main.py
+++ submission8/main.py
@@ -1,3 +1,30 @@
-# Лабораторна робота 8: Основи ООП
+class CalendarRecord:
+    def __init__(self, title, date, time):
+        self.title = title
+        self.date = date
+        self.time = time
+    def edit(self, new_title=None, new_date=None, new_time=None):
+        if new_title is not None:
+            self.title = new_title
+        if new_date is not None:
+            self.date = new_date
+        if new_time is not None:
+            self.time = new_time
+    def __str__(self):
+        return f"📅 {self.date} о {self.time} | {self.title}"
 
-# Реалізуйте завдання тут
+
+def main():
+    record1 = CalendarRecord("Лекція з Баз Даних", "2026-05-15", "10:00")
+    record2 = CalendarRecord("Зустріч по проекту", "2026-05-16", "14:30")
+    print("--- Початкові записи в календарі ---")
+    print(record1)
+    print(record2)
+    print("\n--- Відбулися зміни у розкладі ---")
+    record1.edit(new_time="12:00", new_title="Лекція з Баз Даних (Перенесено)")
+    print("\n--- Оновлені записи в календарі ---")
+    print(record1)
+    print(record2)
+
+if __name__ == "__main__":
+    main()
