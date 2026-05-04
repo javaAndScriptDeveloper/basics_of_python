8652d6c16415e18dcd665647087b8f1c7aa3a6e9
coldat1k <yrasav2007@gmail.com>
2026-05-04 20:17:17 +0300

    fix2

--- submission8/main.py
+++ submission8/main.py
@@ -3,6 +3,7 @@ class CalendarRecord:
         self.title = title
         self.date = date
         self.time = time
+
     def edit(self, new_title=None, new_date=None, new_time=None):
         if new_title is not None:
             self.title = new_title
@@ -10,16 +11,20 @@ class CalendarRecord:
             self.date = new_date
         if new_time is not None:
             self.time = new_time
+
     def __str__(self):
         return f"📅 {self.date} о {self.time} | {self.title}"
 
 def main():
-    record = CalendarRecord("Конференція", "20.03.2026", "10:00")
-    print("--- Початковий запис ---")
-    print(record)
-    record.edit(new_time="14:00")
+    record1 = CalendarRecord("Зустріч", "15.01.2026", "10:00")
+    record2 = CalendarRecord("Конференція", "20.03.2026", "12:00")
+    print("--- Початкові записи ---")
+    print(record1)
+    print(record2)
+    record1.edit(new_time="11:30")
     print("\n--- Після редагування ---")
-    print(record)
+    print(record1)
+    print(record2)
 
 if __name__ == "__main__":
     main()
