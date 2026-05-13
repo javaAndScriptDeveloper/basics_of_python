41dae6de3796eb935768d65c3ededfc49b740773
AleksD1203 <smiley3223337@gmail.com>
2026-05-13 22:03:25 +0300

    Lab9 Variant 4 done, tests passed

--- submission9/main.py
+++ submission9/main.py
@@ -1,3 +1,10 @@
-# Лабораторна робота 9: Регулярні вирази
+﻿import re
 
-# Реалізуйте завдання тут
+pattern = r'\b[A-ZА-ЯІЇЄҐ]\w*'
+
+text_1 = "Hello everyone! Welcome to Python programming. It is a Great experience!"
+text_2 = "це текст у якому немає жодної великої літери."
+
+print("--- Лабораторна робота №9 (Варіант 4) ---")
+print(f"Знайдені слова (Текст 1): {re.findall(pattern, text_1)}")
+print(f"Знайдені слова (Текст 2): {re.findall(pattern, text_2)}")
