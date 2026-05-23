0d3b2e37238044c906d07a616f9a75ad91226d79
kyrashchuk3 <mikolakyrashuk769@gmail.com>
2026-05-23 19:05:13 +0300

    laba5_var4

--- submission5/main.py
+++ submission5/main.py
@@ -1,22 +1,7 @@
-# Дані для завдання
+with open("input.txt", "r", encoding="utf-8") as file:
+    text = file.read()
 
-# Шлях до вхідного файлу
-input_file = "input.txt"
-
-# Шлях до вихідного файлу
-output_file = "output.txt"
-
-# Слово для пошуку (варіант 5)
-word_to_find = "Python"
-
-# Слово для заміни та нове слово (варіант 6)
-word_to_replace = "World"
-replacement_word = "Ukraine"
-
-# Новий рядок для додавання (варіант 7)
-new_line = "Новий рядок додано"
-
-# Новий вміст для перезапису (варіант 10)
-new_content = "Файл перезаписано"
-
-# Реалізуйте завдання тут
+if "Python" in text:
+    print("Так")
+else:
+    print("Ні")
