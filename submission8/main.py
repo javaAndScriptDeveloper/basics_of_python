f85bfd67a78de40dabce3d7c5e6e9fec27c841b0
kyrashchuk3 <mikolakyrashuk769@gmail.com>
2026-05-23 19:30:30 +0300

    laba8_var4

--- submission8/main.py
+++ submission8/main.py
@@ -1,3 +1,24 @@
-# Лабораторна робота 8: Основи ООП
+class Student:
+    def __init__(self, name):
+        self.name = name
+        self.grades = []
 
-# Реалізуйте завдання тут
+    def add_grade(self, grade):
+        self.grades.append(grade)
+
+    def show_grades(self):
+        print("Оцінки студента", self.name + ":")
+        for grade in self.grades:
+            print(grade)
+
+
+name = input()
+count = int(input())
+
+student = Student(name)
+
+for i in range(count):
+    grade = int(input())
+    student.add_grade(grade)
+
+student.show_grades()
